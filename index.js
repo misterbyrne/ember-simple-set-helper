f149de5ee481c9da5cb04495f75319fd5774497c
Adam Byrne <adam.byrne@intercom.io>
2019-10-18 13:07:21 +0100

    Make the transform parallelisable

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ module.exports = {
   name: require('./package').name,
 
   setupPreprocessorRegistry(type, registry) {
-    registry.add('htmlbars-ast-plugin', {
+    let plugin = this._buildPlugin();
+    plugin.parallelBabel = {
+      requireFile: __filename,
+      buildUsing: '_buildPlugin',
+      params: { },
+    };
+    registry.add('htmlbars-ast-plugin', plugin);
+  },
+
+  _buildPlugin() {
+    return {
       name: 'set-placeholder',
       plugin: require('./lib/simple-set-transform'),
       baseDir() {
         return __dirname;
       },
-    });
+    };
   },
 };
